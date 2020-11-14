e2b1c24bded9640d554abd7e414c842d451e7aa5
vinitaran <vinitarane1998@gmail.com>
2020-11-14 15:09:10 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import InfoBox from './InfoBox';
+import Map from "./Map";
+import {Card, CardContent} from "@material-ui/core"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__left">
+        {/**Header */}
+        <Header />
+        {/**InfoBoxs */}
+        <div className="app__stats">
+          <InfoBox title="Corona Virus Cases" cases="+2,300" total="1.2M" />
+          <InfoBox title="Corona Virus Cases" cases="+2,300" total="1.2M" />
+          <InfoBox title="Corona Virus Cases" cases="+2,300" total="1.2M" />
+        </div>
+        
+
+        {/**Table */}
+        {/**Graphs */}
+        {/**Map */}
+        <Map />
+      </div>
+      <Card className="app__right">
+          <CardContent>
+            {/**Table */}
+            <h1>I am the table</h1>
+            {/**Graph */}
+            <h1>I am the graph</h1>
+          </CardContent>
+      </Card>
     </div>
   );
 }
